c724894664e5ecb45c5fad9d950738297161788c
Alex Lustenberg <alexander.lustenberg@ensighten.com>
2016-01-18 14:06:03 -0500

    attemptig to fix spec tests

--- spec/classes/client_spec.rb
+++ spec/classes/client_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
-describe 'ossec' do
+describe 'ossec::client' do
 
   context 'with defaults for all parameters' do
     it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_class('ossec') }
+    it { is_expected.to contain_class('ossec::client') }
   end
 end
--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
-describe 'ossec' do
+describe 'ossec::server' do
 
   context 'with defaults for all parameters' do
     it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_class('ossec') }
+    it { is_expected.to contain_class('ossec::server') }
   end
 end
