d0e8e9efabaa6a3c6ba85ea6a8b6274cf543f396
Jose Luis Ruiz <jose@wazuh.com>
2016-01-30 15:53:42 -0500

    Allow whitelisting of IP addreses

--- manifests/server.pp
+++ manifests/server.pp
@@ -10,6 +10,7 @@ class ossec::server (
   $ossec_email_alert_level             = 7,
   $ossec_ignorepaths                   = [],
   $ossec_scanpaths                     = [ {'path' => '/etc,/usr/bin,/usr/sbin', 'report_changes' => 'no', 'realtime' => 'no'}, {'path' => '/bin,/sbin', 'report_changes' => 'yes', 'realtime' => 'yes'} ],
+  $ossec_white_list                    = [],
   $ossec_local_files                   = {},
   $ossec_emailnotification             = 'yes',
   $ossec_check_frequency               = 79200,
