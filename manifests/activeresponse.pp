ba2f641a9c2162b5f87c7be09d9ec29daa19b81d
Nikolai <nikolai@ialokin.no>
2016-05-04 14:27:53 +0200

    Added support for repeated_offenders in activeresponse.erb and activeresponse.pp.

--- manifests/activeresponse.pp
+++ manifests/activeresponse.pp
@@ -1,10 +1,11 @@
 #Define for a specific ossec active-response
 define ossec::activeresponse(
   $command_name,
-  $ar_location = 'local',
-  $ar_level    = 7,
-  $ar_rules_id = [],
-  $ar_timeout  = 300,
+  $ar_location           = 'local',
+  $ar_level              = 7,
+  $ar_rules_id           = [],
+  $ar_timeout            = 300,
+  $ar_repeated_offenders = false,
 ) {
   require ossec::params
 
