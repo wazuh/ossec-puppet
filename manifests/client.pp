c0acbdeb690ae58bea723c7ef04659c8968058c4
Nikolai <nikolai@ialokin.no>
2016-05-12 13:24:37 +0200

    repeated_offenders needs to be set for each agent. You can now set a minimal activeresponse containing just repeated_offenders by setting class { "ossec::client":   ar_repeated_offenders => '100,200,300,400,500', }

--- manifests/client.pp
+++ manifests/client.pp
@@ -15,6 +15,7 @@ class ossec::client(
   $manage_repo             = true,
   $manage_epel_repo        = true,
   $manage_client_keys      = true,
+  $ar_repeated_offenders   = '',
 ) inherits ossec::params {
   validate_bool(
     $ossec_active_response, $ossec_rootcheck,
@@ -85,9 +86,17 @@ class ossec::client(
     target  => $ossec::params::config_file,
     content => template('ossec/10_ossec_agent.conf.erb'),
     order   => 10,
-    notify  => Service[$ossec::params::agent_service]
   }
 
+  if ( $ar_repeated_offenders != '' ) {
+    concat::fragment { 'repeated_offenders' :
+      target  => $ossec::params::config_file,
+      content => template('ossec/ar_repeated_offenders.erb'),
+      order   => 55,
+      notify  => Service[$ossec::params::agent_service]
+    }
+  }
+  
   concat::fragment { 'ossec.conf_99' :
     target  => $ossec::params::config_file,
     content => template('ossec/99_ossec_agent.conf.erb'),
