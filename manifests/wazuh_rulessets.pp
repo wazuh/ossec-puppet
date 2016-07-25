2e7525f3425f4b09ee2699de87af72c730f11869
Ira Abramov <ira.abramov@ownbackup.com>
2016-07-25 19:13:01 +0300

    Add a basic rule installer manifest

---
+++ manifests/wazuh_rulessets.pp
@@ -0,0 +1,37 @@
+# Puppetize the instructions on http://documentation.wazuh.com/en/latest/ossec_ruleset.html#automatic-installation
+class ossec::wazuh_rulessets {
+
+  file { '/var/cache/wget':
+    ensure => directory
+  } ->
+  file { '/var/ossec/update':
+    ensure => directory
+  } ->
+  file { '/var/ossec/update/rulesets':
+    ensure => directory
+  } ->
+  wget::fetch { 'https://raw.githubusercontent.com/wazuh/ossec-rules/stable/ossec_ruleset.py':
+    destination => '/var/ossec/update/rulesets/',
+    cache_dir   => '/var/cache/wget',
+    mode        => '0755',
+    verbose     => false,
+    notify      => Exec['get ossec_ruleset']
+  }
+
+  exec { 'get ossec_ruleset':
+    command     => '/var/ossec/update/rulesets/ossec_ruleset.py -s',
+    cwd         => '/var/ossec/update/rulesets/',
+    refreshonly => true
+  }
+
+  cron { 'cron_update_rules':
+    command => 'cd /var/ossec/update/ruleset && ./ossec_ruleset.py -s',
+    user    => 'root',
+    weekday => '6',
+    hour    => '3',
+    minute  => '23'
+  }
+}
+
+
+
