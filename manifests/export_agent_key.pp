ec94e182fb32f92b97d30c0b3b98e9bdeb78477b
Jos Luis Ruiz <jose@wazuh.com>
2017-02-06 14:17:18 -0600

    update CHANGELOG

--- manifests/export_agent_key.pp
+++ manifests/export_agent_key.pp
@@ -1,3 +1,4 @@
+#Export agent key
 class ossec::export_agent_key($max_clients, $agent_name, $agent_ip_address) {
   ossec::agentkey{ "ossec_agent_${agent_name}_client":
     agent_id         => fqdn_rand($max_clients),
