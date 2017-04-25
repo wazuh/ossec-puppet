c42a6b49f5a131f8c6412057199921b2fd345688
Jose Luis Ruiz <jose@wazuh.com>
2017-04-25 00:00:48 -0500

    fix some puppet-lint typo and update changelog and metadata

--- manifests/agentkey.pp
+++ manifests/agentkey.pp
@@ -9,13 +9,13 @@ define ossec::agentkey(
 
   if ! $agent_id { fail("ossec::agentkey: ${agent_id} is missing")}
 
-  $agentKey1 = md5("${agent_id} ${agent_seed}")
-  $agentKey2 = md5("${agent_name} ${agent_ip_address} ${agent_seed}")
+  $agentkey1 = md5("${agent_id} ${agent_seed}")
+  $agentkey2 = md5("${agent_name} ${agent_ip_address} ${agent_seed}")
 
   concat::fragment { "var_ossec_etc_client.keys_${agent_name}_part":
     target  => $ossec::params::keys_file,
     order   => $agent_id,
-    content => "${agent_id} ${agent_name} ${agent_ip_address} ${agentKey1}${agentKey2}\n",
+    content => "${agent_id} ${agent_name} ${agent_ip_address} ${agentkey1}${agentkey2}\n",
   }
 
 }
